2392ff1165d2ed430b5e2f1d588c9049f82a335c
Victor Premoli <victor.premoli57@gmail.com>
2020-12-17 23:42:59 -0300

    adding update form page

--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -1,3 +1,5 @@
+const {formatPrice} = require("../../lib/utils")
+
 const Category = require("../models/Category")
 const Product = require("../models/Product")
 
@@ -13,7 +15,6 @@ module.exports = {
         })
     },
     async post(req,res) {
-
         //Lógica de salvar
         const keys = Object.keys(req.body)
 
@@ -22,16 +23,27 @@ module.exports = {
                 return res.send("please fill all fildes!")
             }
         }
+
         let results = await Product.create(req.body)
         const productId = results.rows[0].id
+
+        return res.redirect(`/products/${productId}`)
+
+    },
+    async edit (req, res) {
+        let results = await Product.find(req.params.id)
+        const product = results.rows[0]
+
+        if(!product) return res.send("product not found!")
+
+        product.old_price = formatPrice(product.old_price)
+        product.price = formatPrice(product.price)
 
         results = await Category.all()
         const categories = results.rows
 
 
 
-        return res.render("products/create.njk", { productId, categories })
-
-
+        return res.render("products/edit.njk", { product, categories })
     }
 }
