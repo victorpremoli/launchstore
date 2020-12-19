18308e1df7ce59c0eea67c95988caaee9fcbb1d9
Victor Premoli <victor.premoli57@gmail.com>
2020-12-18 22:32:39 -0300

    features of update and delete products

--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -42,8 +42,33 @@ module.exports = {
         results = await Category.all()
         const categories = results.rows
 
-
-
         return res.render("products/edit.njk", { product, categories })
+    },
+    async put (req,res) {
+        const keys = Object.keys(req.body)
+
+        for (const key of keys) {
+            if(req.body[key] == "") {
+                return res.send(`please fill all fildes! ${key}`)
+            }
+        }
+
+        req.body.price = req.body.price.replace(/\D/g,"")
+
+        if (req.body.price != req.body.old_price) {
+            const oldProduct = await Product.find(req.body.id)
+
+            req.body.old_price = oldProduct.rows[0].price
+        }
+
+        await Product.update(req.body)
+
+        return res.redirect(`/products/${req.body.id}/edit`)
+
+    },
+    async delete(req,res) {
+        await Product.delete(req.body.id)
+        
+        return res.redirect('/products/create')
     }
 }
